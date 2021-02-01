5b8d396081121ef41263d45649dae7a6f958da47
Matias Facio <matiaspersonal@gmail.com>
2021-02-01 09:07:41 +0200

    updated packages

--- tango-def/src/helpers/useSearch.js
+++ tango-def/src/helpers/useSearch.js
@@ -1,18 +1,18 @@
 import { useState } from "react";
 
 const useSearch = () => {
-    const [searchResult, setSearchResult] = useState('')
+  const [searchResult, setSearchResult] = useState("");
 
-    const startSearch = (props) =>{
-        fetch(`/search/${props}`)
-        .then(response => response.json())
-        .then(data => setSearchResult(data))
-        .catch(err => console.log(err))
+  const startSearch = (props) => {
+    fetch(`/search/${props}`)
+      .then((response) => response.json())
+      .then((data) => setSearchResult(data))
+      .catch((err) => console.log(err));
 
-        console.log('this search over here:', searchResult);
-    }
+    console.log("this search over here:", searchResult);
+  };
 
-    return [searchResult, startSearch];
-}
- 
+  return [searchResult, startSearch];
+};
+
 export default useSearch;
